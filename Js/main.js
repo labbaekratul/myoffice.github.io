0c2ea28c99a3f57fb7a6a0e3e5d470a5fdc905bd
Labbaek ratul <labbaekratul@gmail.com>
2021-02-07 11:14:40 -0800

    my office website

---
+++ Js/main.js
@@ -0,0 +1,29 @@
+// ****BX SIDER
+
+
+//$(document).ready(function(){
+  // $('.slider_area').bxSlider();
+   
+
+  //});
+
+  $('.slider_area').bxSlider({
+    auto: true,
+  
+   
+  });
+
+
+
+    $('.navbar').meanmenu({
+     meanManuConatainer : '#mobile_menu',
+     meanScreenWidth : 768,
+    
+
+
+  });
+
+
+
+
+ 
